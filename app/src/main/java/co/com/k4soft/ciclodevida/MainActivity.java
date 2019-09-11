237aa9cac2e198f7551435e708aeb0af6d1d077d
diegogarcia <consultor@dgpragmatic.com>
2019-09-11 13:22:55 -0500

    Initial commit

---
+++ app/src/main/java/co/com/k4soft/ciclodevida/MainActivity.java
@@ -0,0 +1,72 @@
+package co.com.k4soft.ciclodevida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void goToActivity2(View view) {
+        Intent intent = new Intent(this,Vista2Activity.class);
+        startActivity(intent);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
+        Log.d("->","onCreate()");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_LONG).show();
+        Log.d("->","onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_LONG).show();
+        Log.d("->","onResume()");
+    }
+
+    @Override
+    public void onPause() {
+
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_LONG).show();
+        Log.d("->","onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_LONG).show();
+        Log.d("->","onStop()");
+    }
+
+    @Override
+    public void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_LONG).show();
+        Log.d("->","onRestart()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_LONG).show();
+        Log.d("->","onDestroy()");
+    }
+
+
+
+}
---
+++ app/src/main/java/co/com/k4soft/ciclodevida/Vista2Activity.java
@@ -0,0 +1,14 @@
+package co.com.k4soft.ciclodevida;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vista2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vista2);
+    }
+}
